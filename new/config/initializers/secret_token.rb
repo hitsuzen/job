160c2430eecca1eb88c7b94858048f02c347f1c3
Rud <rud@outlook.com>
2016-04-15 18:49:30 -0300

    clone inteiro

---
+++ new/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teste::Application.config.secret_token = '8b4d11bedaf9bb7d01bf44f0d9209ea01c3891edd905fb8d9461d032bbe7cf5644d9926ceb588dbd8235e92135cf9ca9eac194d98e27d764f6f69b12d9fc7a55'
